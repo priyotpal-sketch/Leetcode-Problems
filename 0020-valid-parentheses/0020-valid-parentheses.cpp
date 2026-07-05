7052a1f9e291e84079a558b6a82561322793a0c2
p_utpal@2301 <priyotpal@gmail.com>
2026-07-05 12:48:50 +0530

    Time: 0 ms (100%), Space: 9 MB (37.87%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        stack<char> s;
+
+        for(int i = 0; i < str.length(); i++) {
+
+            if(str[i] == '(' || str[i] == '[' || str[i] == '{') {
+                s.push(str[i]);
+            }
+            else {
+                if(s.empty()) return false;
+
+                if((str[i] == ')' && s.top() != '(') ||
+                   (str[i] == ']' && s.top() != '[') ||
+                   (str[i] == '}' && s.top() != '{')) {
+                    return false;
+                }
+
+                s.pop();
+            }
+        }
+
+        return s.empty();
+    }
+};
