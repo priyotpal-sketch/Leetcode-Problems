6cdb5336f0ae73ac74083834a6089b2d2651f20a
p_utpal@2301 <priyotpal@gmail.com>
2026-08-26 15:55:28 +0530

    Time: 0 ms (100%), Space: 10.1 MB (70.77%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,26 @@
+// Time complexity of this approach will be O(n^2) , we have to transpose first then reverse the rows 
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+
+        int n = matrix.size();
+
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                swap(matrix[i][j], matrix[j][i]);
+
+            }
+        }
+
+
+        
+        for (int i = 0; i < n; i++) {
+
+            reverse(matrix[i].begin(), matrix[i].end());
+
+        }
+    }
+};
