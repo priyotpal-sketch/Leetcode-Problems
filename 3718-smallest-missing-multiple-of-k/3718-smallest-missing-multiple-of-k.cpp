e05aa032cc60addccd5fffa9ec4bcb7c50f4bae5
p_utpal@2301 <priyotpal@gmail.com>
2026-08-26 16:27:11 +0530

    Time: 3 ms (38.04%), Space: 25.1 MB (44.62%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,16 +1,15 @@
-// Time complexity will be O(n)
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
 
         unordered_set<int> st;
 
-        
+        // Store all numbers
         for(int x : nums) {
             st.insert(x);
         }
 
-    
+        // Check multiples of k
         int multiple = k;
 
         while(true) {
