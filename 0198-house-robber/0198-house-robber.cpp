231e45f0086f024dcfee481c1da86d35787b0d33
p_utpal@2301 <priyotpal@gmail.com>
2026-09-06 23:09:53 +0530

    Time: 0 ms (100%), Space: 10.7 MB (46.68%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,25 @@
+// Since we are visitng each house only once which will lead to O(n) time complexity and we have also created a array dp which will take space complexityO(n)
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+
+        int n = nums.size();
+
+        if (n == 1)
+            return nums[0];
+
+        vector<int> dp(n);
+
+        dp[0] = nums[0];
+
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+
+            dp[i] = max(dp[i - 1],
+                        dp[i - 2] + nums[i]);
+        }
+
+        return dp[n - 1];
+    }
+};
